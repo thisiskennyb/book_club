4fafbbbc8e7b18de67a3a888178239084bb3d4f2
Ken Bonnette <117300139+thisiskennyb@users.noreply.github.com>
2023-11-02 19:10:47 -0400

    minor changes

--- backend/book/models.py
+++ backend/book/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class Book(models.Model):
-    author = models.CharField()
-    title = models.CharField()
+    author = models.CharField(max_length=60)
+    title = models.CharField(max_length=60)
     pages = models.IntegerField()
-    book_cover_id = models.CharField(null=True)
-    
+    book_cover_id = models.CharField(null=True, max_length=60)
+    ratings = models.DecimalField()
+    first_sentence = models.CharField(null=True, max_length=255)
 
 
 
