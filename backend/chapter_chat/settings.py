e8b2bd808373ddbf8e20d3b95ec34c9726a724eb
Noelbiggs28 <noelbiggs28@gmail.com>
2023-11-25 12:24:41 -1000

    fixed it on my computer

--- backend/chapter_chat/settings.py
+++ backend/chapter_chat/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l-d%n8!jz^$1c@&47e8--m6!@7n6e$p_=y&05922^eib1g=7&5'
-# SECRET_KEY = os.getenv("SECRET_KEY") 
+# SECRET_KEY = 'django-insecure-l-d%n8!jz^$1c@&47e8--m6!@7n6e$p_=y&05922^eib1g=7&5'
+SECRET_KEY = os.getenv("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG =  True if os.getenv("DEBUG") == "True" else False
+# DEBUG = True
+DEBUG =  True if os.getenv("DEBUG") == "True" else False
 
 
 
@@ -87,11 +87,14 @@ WSGI_APPLICATION = 'chapter_chat.wsgi.application'
 DATABASES = {
 "default": {
 "ENGINE": "django.db.backends.postgresql",
+# "NAME": os.getenv('DB_NAME'),
+# "USER": os.getenv('DB_USER'),
+# "PASSWORD": os.getenv('DB_PASS'),
+# "HOST": "db",
+# "PORT": 5432, 
 "NAME": "book_db",
 "USER": "postgres",
 "PASSWORD": "postgres",
-# "HOST": "db",
-# "PORT": 5432, 
 "HOST": "localhost",
 "PORT": 5454, 
 }
