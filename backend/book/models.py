04c3bcd48877c8289a9870cf674144bda14329b7
Noelbiggs28 <noelbiggs28@gmail.com>
2023-12-02 06:34:40 -1000

    can no add books with no covers

--- backend/book/models.py
+++ backend/book/models.py
@@ -5,18 +5,19 @@ class Book(models.Model):
     author = models.CharField()
     title = models.CharField()
     pages = models.IntegerField()
-    book_cover_id = models.CharField(null=True)
+    book_cover_id = models.CharField(null=True, default=None, blank=True)
     open_library_id = models.CharField(default=None)
 
 
     @classmethod
     def get_or_create_book(cls, book_data):
         open_library_id = book_data['book']['open_library_id']
+        book_cover_id = book_data.get('book_cover_id', None)
         book, created = cls.objects.get_or_create(open_library_id=open_library_id, defaults={
             'author': book_data['book']['author'],
             'title': book_data['book']['title'],
             'pages': book_data['book']['pages'],
-            'book_cover_id': book_data['book']['book_cover_id'],
+            'book_cover_id': book_cover_id,
         
         })
         return book
