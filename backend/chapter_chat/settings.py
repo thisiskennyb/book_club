72f19fe94bdee5c90fd55a17c9764f4307f8e05a
Ken Bonnette <117300139+thisiskennyb@users.noreply.github.com>
2023-12-02 15:03:09 -0500

    set up pipeline

--- backend/chapter_chat/settings.py
+++ backend/chapter_chat/settings.py
@@ -111,8 +111,10 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME", "book_db"),
         "USER": os.getenv("DB_USER", "postgres"),
         "PASSWORD": os.getenv("DB_PASS", "postgres"),
-        "HOST": "db" if os.getenv("DB_NAME") else "localhost",
-        "PORT": 5432 if os.getenv("DB_NAME") else 5454
+        # "HOST": "db" if os.getenv("DB_NAME") else "localhost",
+        # "PORT": 5432 if os.getenv("DB_NAME") else 5454
+        "HOST": "db",
+        "PORT": 5432
     }
 }
 
