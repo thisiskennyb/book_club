e97f356b1e7a6d6dbf78b065fad179ca92ba73b1
tomiwa <tmakinbayo@gmail.com>
2023-12-04 18:48:20 -0600

    DockerUserName

--- backend/book_list/tests.py
+++ backend/book_list/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+# from django.test import TestCase
+# from django.contrib.auth.models import User
+# # from django.urls import 
+# from rest_framework import status
+# from rest_framework.test import APIClient
+# from .models import ToBeRead, CompletedBook
+# from book.models import Book
+
+# class CompletedViewTests(TestCase):
+#   def setUp(self):
+#     self.user = User.objects.create(username='testuser')
+#     self.completed_book = CompletedBook.objects.create(
+#       user_profile = 'Test User',
+#       book= 'Test Book',
+#       user_rating = 5,
+#       recommended = True
+#     )
+
+#   def test_completed(self):
+#     self.assertEqual(self.completed_book.user_profile, 'Test User')
+#     self.assertEqual(self.completed_book.book, 'Test Book')
+#     self.assertEqual(self.completed_book.user_rating, 5)
+#     self.assertEqual(self.completed_book.recommended, True)
+
+
+
+
 
 # Create your tests here.
